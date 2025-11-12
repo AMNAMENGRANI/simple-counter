49fdea2cc5ee872dceb177c260bb9dfd8928122e
Amna Mengrani <162323996+AMNAMENGRANI@users.noreply.github.com>
2025-11-12 19:51:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let count = 0;
+function increment(){
+    count++;
+    document.getElementById("counter").textContent=count;
+}
+function decrement(){
+    count--;
+    document.getElementById("counter").textContent=count;
+}
+function reset(){
+    count=0;
+    document.getElementById("counter").textContent=count;
+}
